fd11131ea541b3e52526998f95db78d98ad5f7fd
Gheve Mombay <gheve.mombay97@gmail.com>
2025-03-03 21:44:27 +0800

    Added a text Feature movies

--- src/app/m/home/page.js
+++ src/app/m/home/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Home() {
     return ( 
-    <div className="bg-slate-600 w-full h-screen">
-        <h1>Hello World</h1>
+    <div className="bg-slate-600 w-full h-screen ">
+        <h1 className="font-bold text-2xl pt-8 pl-5">Feature Movies</h1>
     </div> );
 }
